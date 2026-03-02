ae568da4f469dc5d50470ebdf2539424fc10016a
manmen2414 <inotitokokoro@gmail.com>
2026-03-02 14:32:53 +0900

    [Add] Youtube埋め込み再生機能の追加

--- src/subekashi-page.js
+++ src/subekashi-page.js
@@ -40,14 +40,81 @@ const _pageScripts = [];
       );
     freeNumberSettingForId("is_shown_new");
     freeNumberSettingForId("is_shown_lack");
+
+// Song page
+(() => {
+  const path = "songs";
+  /**@param {string} url  */
+  //TODO: どうにかして関数をインジェクトする
+  //@ts-ignore
+  exportFunction(
+    /**@param {string} url */
+    (url) => {
+      closeYoutubeVideo();
+      const regExec = /youtu\.be\/([a-zA-Z0-9_\-]+)/.exec(url);
+      if (!regExec) {
+        showToast("error", "このリンクはYoutubeのものではありません。");
+        return;
+      }
+      const tr = document.createElement("tr");
+      tr.id = "youtube-area";
+      tr.innerHTML = `<td><i class="fas fa-window-close" onclick="_sbksAddonCloseYoutubeVideo()"></i>プレイヤー</td><td><iframe width="448" height="252" src="https://www.youtube.com/embed/${regExec[1]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></td>`;
+      const info = document.getElementById("song-info");
+      if (!info)
+        return void showToast(
+          "error",
+          "再生するフレームの作成に失敗しました。",
+        );
+      info.insertAdjacentElement("beforeend", tr);
+    },
+    window,
+    { defineAs: "_sbksAddonPlayYoutubeVideo" },
+  );
+  const closeYoutubeVideo = () => {
+    const tr = document.querySelector("#youtube-area");
+    if (!tr) return;
+    tr.remove();
+  };
+  //@ts-ignore
+  exportFunction(closeYoutubeVideo, window, {
+    defineAs: "_sbksAddonCloseYoutubeVideo",
+  });
+  function page() {
+    const youtubeLinks = Array.from(
+      document.querySelectorAll(".song-url>.fa-youtube"),
+    )
+      .map((el) => el.parentElement)
+      .filter((el) => !!el && el instanceof HTMLAnchorElement);
+    youtubeLinks.forEach((a) => {
+      if (!a.parentElement) return;
+      a.parentElement.style.display = "flex";
+      const playButtonIcon = document.createElement("i");
+      playButtonIcon.className = `fas fa-play`;
+      const playButton = document.createElement("button");
+      playButton.setAttribute(
+        "onclick",
+        `_sbksAddonPlayYoutubeVideo("${a.href}")`,
+      );
+      playButton.append(playButtonIcon);
+      playButton.style.background = "none";
+      playButton.style.width = "fit-content";
+      playButton.style.height = "fit-content";
+      playButton.style.margin = "0";
+      playButton.style.padding = "3px";
+      a.insertAdjacentElement("beforebegin", playButton);
+    });
+  }
+  _pageScripts.push({ path, page });
+})();
+
   }
   _pageScripts.push({ path, page });
 })();
 
 // Get Page
 (() => {
-  // 最初と最後の/がないパス
-  const thisPath = location.pathname.replace(/(^\/)|(\/$)/g, "");
+  // 最初のパス
+  const thisPath = location.pathname.replace(/(^\/)|(\/$)/g, "").split("/")[0];
   const pageScript = _pageScripts.find(({ path }) => path === thisPath);
   if (!pageScript) return;
   pageScript.page();
