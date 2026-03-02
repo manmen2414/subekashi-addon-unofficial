46fcb503f79b3cf2079af03831f88ea46dd34941
manmen2414 <inotitokokoro@gmail.com>
2026-03-02 14:33:15 +0900

    [Add] 未審査・未通過広告の表示設定を追加

--- src/subekashi-page.js
+++ src/subekashi-page.js
@@ -31,6 +31,66 @@ const _pageScripts = [];
     if (!label) throw new Error(`No ${id} setting label`);
     label.innerText = `${label.innerText}※`;
   }
+  async function aboutAdSetting() {
+    const settingSection = document.querySelector("#mainarticle>section");
+    if (!settingSection) return;
+    // 移動
+    const isShownAd = document.getElementById("is_shown_ad");
+    if (!isShownAd || !(isShownAd instanceof HTMLSelectElement)) return;
+    const shownAdParent = isShownAd.parentElement;
+    if (!shownAdParent) return;
+    const adColomnTitle = document.createElement("h2");
+    adColomnTitle.innerText = "トップ画面の宣伝";
+    settingSection.append(adColomnTitle, shownAdParent);
+    // 設定の追加
+    const showUnpassedNow =
+      ((await cookieStore.get("show_unpassed")) ?? {}).value ?? "off";
+    const showUnpassedWrapper = document.createElement("div");
+    showUnpassedWrapper.className = "form-col";
+    showUnpassedWrapper.innerHTML = `<label>未審査・未通過</label>
+    <select id="show_unpassed" class="setting-input">
+    <option value="off" ${showUnpassedNow === "off" ? "selected" : ""}>非表示</option>;
+    <option value="still" ${showUnpassedNow === "still" ? "selected" : ""}>未審査を表示</option>
+    <option value="fail" ${showUnpassedNow === "fail" ? "selected" : ""}>未審査と未通過を表示</option>
+    </select>`;
+    settingSection.append(showUnpassedWrapper);
+    const showUnpassed = document.getElementById("show_unpassed");
+    if (!showUnpassed) return;
+    if (!(showUnpassed instanceof HTMLSelectElement)) return;
+    showUnpassed.setAttribute(
+      "onchange",
+      "cookieStore.set('show_unpassed',event.target.value)",
+    );
+    // 一部設定では自前の宣伝処理を行うため、「宣伝の表示」について補正処理を入れる
+    isShownAd.setAttribute(
+      "onchange",
+      "cookieStore.set('is_shown_ad_addoned',event.target.value)",
+    );
+    isShownAd.value =
+      ((await cookieStore.get("is_shown_ad_addoned")) ?? {}).value ?? "on";
+    setInterval(() => {
+      calcClientAdRendering();
+    }, 50);
+    async function calcClientAdRendering() {
+      // 拡張済み「宣伝の表示」がonかつ自前の処理を行うオプションがonなら、本来の「宣伝の表示」をoffにする
+      const isShownAdAddonedValue =
+        ((await cookieStore.get("is_shown_ad_addoned")) ?? {}).value === "on";
+      const needClientAdRenderingOptions = [
+        ((await cookieStore.get("show_unpassed")) ?? {}).value !== "off",
+      ];
+      if (
+        isShownAdAddonedValue &&
+        needClientAdRenderingOptions.some((v) => v)
+      ) {
+        await cookieStore.set("is_shown_ad", "off");
+      } else {
+        await cookieStore.set(
+          "is_shown_ad",
+          isShownAdAddonedValue ? "on" : "off",
+        );
+      }
+    }
+  }
   function page() {
     document
       .querySelector("#mainarticle>section>h2")
@@ -40,6 +100,10 @@ const _pageScripts = [];
       );
     freeNumberSettingForId("is_shown_new");
     freeNumberSettingForId("is_shown_lack");
+    aboutAdSetting();
+  }
+  _pageScripts.push({ path, page });
+})();
 
 // Song page
 (() => {
@@ -107,6 +171,86 @@ const _pageScripts = [];
   _pageScripts.push({ path, page });
 })();
 
+// Top Page
+(() => {
+  const path = "";
+  async function selectRandomAdContent(
+    allowStill = false,
+    allowFailed = false,
+  ) {
+    const res = await fetch("https://lyrics.imicomweb.com/api/ad/");
+    /**@type {{id:number,url:string,status:"fail"|"pass"|"still",view:number,dup:number}[]} */
+    let json = await res.json();
+    json = json.filter((ad) => ad.status === "still");
+    json = json.filter((ad) => {
+      if (!allowStill && ad.status === "still") return false;
+      if (!allowFailed && ad.status === "fail") return false;
+      return true;
+    });
+    const allAdDupLong = json.reduce((num, { dup }) => num + dup, 0);
+    let selectAdDup = Math.floor(Math.random() * allAdDupLong);
+    const selectedAd = json.find((ad) => {
+      selectAdDup -= ad.dup;
+      if (selectAdDup < 0) return true;
+    });
+    if (!selectedAd) return;
+    return selectedAd;
+  }
+  async function showAd() {
+    /**@type {"off"|"still"|"fail"} */
+    //@ts-ignore
+    const showUnpassed =
+      ((await cookieStore.get("show_unpassed")) ?? {}).value ?? "off";
+    let htmlText = `
+    <h1>宣伝</h1>
+    <div class="underline"></div>
+    <div class="youtube" id="ad">
+      <iframe id="player" src="https://www.youtube-nocookie.com/embed/%id%?enablejsapi=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreenframeborder="0" data-gtm-yt-inspected-8="true"></iframe>
+    </div>
+    <div class="dummybuttons">
+      <a href="/ad/"><div class="dummybutton"><p>宣伝する</p></div></a>
+      <a href="/setting/#is_shown_ad"><div class="dummybutton"><p>非表示にする</p></div></a>
+    </div>
+    `;
+    const selectedAd = await selectRandomAdContent(
+      showUnpassed !== "off",
+      showUnpassed === "fail",
+    );
+    if (!selectedAd) return;
+    const regExec = /youtu\.be\/([a-zA-Z0-9_\-]+)/.exec(selectedAd.url);
+    if (!regExec) return;
+    htmlText = htmlText.replaceAll("%id%", regExec[1]);
+
+    const adObject = document.createElement("div");
+    adObject.innerHTML = htmlText;
+    const h1s = Array.from(document.getElementsByTagName("h1"));
+    const adNextItemHeader =
+      h1s.find(({ innerText }) => innerText === "未完成") ??
+      h1s.find(
+        ({ innerText }) => innerText === "未完成" || innerText === "リンク",
+      );
+    if (!adNextItemHeader) return;
+    adNextItemHeader.insertAdjacentElement("beforebegin", adObject);
+    // 公開中の宣伝であればviewを増加
+    if (selectedAd.status === "pass")
+      await fetch(`https://lyrics.imicomweb.com/api/ad/${selectedAd.id}`, {
+        body: JSON.stringify({
+          view: selectedAd.view + 1,
+        }),
+        method: "PUT",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      });
+  }
+
+  async function page() {
+    if (
+      ((await cookieStore.get("is_shown_ad_addoned")) ?? {}).value === "on" &&
+      ((await cookieStore.get("is_shown_ad")) ?? {}).value === "off"
+    ) {
+      showAd();
+    }
   }
   _pageScripts.push({ path, page });
 })();
